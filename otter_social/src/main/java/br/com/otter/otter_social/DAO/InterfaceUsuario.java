82b07726f0af3aea9ce2fb15eb2369c602ffe9b3
Guilherme Rodrigues <rsilvaguilherme846@gmail.com>
2024-03-13 12:55:57 -0300

    Apagando arquivos

--- otter_social/src/main/java/br/com/otter/otter_social/DAO/InterfaceUsuario.java
+++
@@ -1,8 +0,0 @@
-package br.com.otter.otter_social.DAO;
-
-import br.com.otter.otter_social.model.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InterfaceUsuario extends CrudRepository<Usuario, Integer> {
-
-}
